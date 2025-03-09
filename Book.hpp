55cba36e84fab61f939015c750666daabe48801e
Sai Koushik Ch.V <saikoushik.chundi@gmail.com>
2025-03-09 14:17:26 +0530

    Update Book.hpp

--- Book.hpp
+++ Book.hpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+//Forward declaration of Student and Faculty to avoid circular dependency.
+class Student;
+class Faculty;
+
 // Status enumeration.
 enum class Status { AVAILABLE, BORROWED, RESERVED };
 
@@ -19,6 +23,10 @@ private:
     string ISBN;
     Status status;
     string book_id;
+    // Extra attributes for reservation.(not displayed anywhere)
+    string reservedBy_studid; // *It stores the student ID who reserved the book.*
+    string reservedBy_facid;  // *It stores the faculty ID who reserved the book.*
+
 public:
     // Constructor declaration.
     Book(string title, string author, string publisher, int Year, string b_id, string isbn);
@@ -31,6 +39,8 @@ public:
     string get_ISBN() const;
     Status get_status() const;
     string get_bookid() const;
+    string get_reservedBy_studid() const;
+    string get_reservedBy_facid() const;
 
     // Setters.
     void set_Title(const string &title);
@@ -40,10 +50,11 @@ public:
     void set_ISBN(const string &isbn);
     void set_status(Status newStatus);
     void set_bookid(const string &id);
+    void set_reservedBy_studid(string studid);
+    void set_reservedBy_facid(string facid);
 
     // Other operations.
     void viewDetails() const;
 };
 
 #endif // BOOK_HPP
-
