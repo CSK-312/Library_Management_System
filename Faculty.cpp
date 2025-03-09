cef65992fbcc4c6e9ad53ec0bb300f96fce7e543
Sai Koushik Ch.V <saikoushik.chundi@gmail.com>
2025-03-09 14:17:55 +0530

    Update Faculty.cpp

--- Faculty.cpp
+++ Faculty.cpp
@@ -1,5 +1,5 @@
 #include "Faculty.hpp"
-#include "Library.hpp" // Full definition of Library is required here
+#include "Library.hpp"
 
 Faculty::Faculty(const string &name, const string &id, const string &pwd) {
     this->Name = name;
@@ -8,6 +8,7 @@ Faculty::Faculty(const string &name, const string &id, const string &pwd) {
     this->fine_rate = 0;
     this->user_id = id;
     this->password = pwd;
+    notif = "";
 }
 
 
@@ -15,8 +16,50 @@ void Faculty::View_all_books(Library *library) const {
     library->show_all_books();
 }
 
+void Faculty::Reserve_book(const string &book_id, Library *library) {
+    Book *book = library->get_book(book_id);
+    if (!book) {
+        cout << "Book not found in the library." << endl;
+        return;
+    }
+    if (book->get_status() == Status::RESERVED) {
+        if(book->get_reservedBy_facid() == this->get_user_id()) {
+            cout << "You have already reserved this book." << endl;
+            return;
+        }
+        cout << "The book is already reserved." << endl;
+        return;
+    }else if(book->get_status() == Status::AVAILABLE) {
+        book->set_reservedBy_facid(this->get_user_id());
+        cout << "Book reserved successfully." << endl;
+        book->set_status(Status::RESERVED);
+        return;
+    }else{
+        // If the book is borrowed but is reserved for next usage.
+        if(!book->get_reservedBy_facid().empty() || !book->get_reservedBy_studid().empty()) {
+            if(book->get_reservedBy_facid() == this->get_user_id()) {
+                cout << "You have already reserved this book." << endl;
+                return;
+            }
+            cout << "The book is reserved by another user." << endl;
+            return;
+        }else{
+            book->set_reservedBy_facid(this->get_user_id());
+            cout << "Book reserved successfully." << endl;
+            // Here we dont change the status to reserved because the book is borrowed.
+            return;
+        }
+    }
+}
 
 void Faculty::Borrow_book(const string &book_id, Library *library) {
+    // If the faculty has this book in borrowed list, then he/she cannot borrow it again.
+    for(const auto &record : acc.getBorrowedBooks()) {
+        if(record.first->get_bookid() == book_id) {
+            cout << "You have already borrowed this book." << endl;
+            return;
+        }
+    }
     bool hasOverdue = acc.hasOverdueBooks(maxBorrowPeriod);
 
     // If user is faculty but has overdue books, block.
@@ -41,7 +84,28 @@ void Faculty::Borrow_book(const string &book_id, Library *library) {
     }
     // Check if the book is available.
     if (book->get_status() == Status::BORROWED) {
-        cout << "The book is currently unavailable." << endl;
+        // If the book is borrowed, ask if the user wants to reserve it for next usage.
+        cout << "The book is currently borrowed." << endl;
+        cout << "You can reserve the book if you want." << endl;
+        cout << "1. Reserve the book" << endl;
+        cout << "2. Cancel" << endl;
+        int choice;
+        cin >> choice;
+        cin.ignore();
+        if (choice == 1) {
+            // Reserve the book.
+            Reserve_book(book_id, library);
+        }
+        return;
+    }else if(book->get_status() == Status::RESERVED) {
+        if(book->get_reservedBy_facid() == this->get_user_id()) {
+            acc.addBorrowedBook(book);
+            book->set_reservedBy_facid("");
+            book->set_status(Status::BORROWED);
+            cout << "Book borrowed successfully." << endl;
+            return;
+        }
+        cout << "The book is reserved by another user." << endl;
         return;
     }
 
@@ -58,17 +122,11 @@ void Faculty::Return_book(const string &book_id, Library *library) {
         return;
     }
     // Ask Account how many days the book was borrowed.
-    int daysBorrowed = acc.removeBorrowedBookAndReturnDaysBorrowed(book_id);
+    int daysBorrowed = acc.getDaysBorrowed(book_id);
     if (daysBorrowed == -1) {
         cout << "Book not found in your borrowed list." << endl;
         return;
     }
-
-    // Mark the book as available.
-    book->set_status(Status::AVAILABLE);
-
-    // Show success message.
-    cout << "Book returned successfully." << endl;
 
     // Overdue check for this specific book.
     int daysOverdue = daysBorrowed - maxBorrowPeriod;
@@ -83,6 +141,28 @@ void Faculty::Return_book(const string &book_id, Library *library) {
     if (hasOverdue) {
         cout << "You still have overdue books. Please return them before borrowing new books." << endl;
     }
+    
+    // If the book is reserved by someone else notify them.
+    if(!book->get_reservedBy_facid().empty() || !book->get_reservedBy_studid().empty()) {
+        book->set_status(Status::RESERVED);
+        string n = "The book you reserved (ID: " + book->get_bookid() + ", Title: " + book->get_Title() + ") is now available.";
+        // Notify the user who reserved the book.
+        Student* stud = library->get_student(book->get_reservedBy_studid());
+        Faculty* fac = library->get_faculty(book->get_reservedBy_facid());
+        if(stud != nullptr) {
+            stud->set_notif(n);
+            book->set_reservedBy_studid("");
+        }else if(fac != nullptr) {
+            fac->set_notif(n);
+            book->set_reservedBy_facid("");
+        }
+        cout << "Book returned successfully." << endl;
+        return;
+    }
+
+    // Mark the book as available.
+    book->set_status(Status::AVAILABLE);
+    cout << "Book returned successfully." << endl;
 }
 
 void Faculty::View_borrowed_books() const {
@@ -111,24 +191,28 @@ void Faculty::View_borrowed_books() const {
 
 void Faculty::View_borrowing_history() const {
     if(acc.getBorrowingHistory().empty()) {
-        std::cout << "You have not borrowed any books in the past." << std::endl;
+        std::cout << "You have no borrowing history." << std::endl;
         return;
     }
     std::cout << "Your borrowing history: " << std::endl;
     std::cout << std::string(65, '-') << std::endl;
     std::cout << std::left << std::setw(15) << "Book ID" 
               << std::setw(30) << "Title" 
+              << std::setw(20) << "Borrowed on" 
               << std::setw(20) << "Returned on" 
               << std::endl;
     std::cout << std::string(65, '-') << std::endl;
 
     for (const auto &record : acc.getBorrowingHistory()) {
         Book* book = record.first;
-        time_t returnTime = record.second;
-        tm* localTime = localtime(&returnTime);
+        time_t borrowTime = record.second.first;
+        time_t returnTime = record.second.second;
+        tm* localBorrowTime = localtime(&borrowTime);
+        tm* localReturnTime = localtime(&returnTime);
         std::cout << std::left << std::setw(15) << book->get_bookid()
                   << std::setw(30) << book->get_Title()
-                  << std::setw(20) << std::put_time(localTime, "%Y-%m-%d")
+                  << std::setw(20) << std::put_time(localBorrowTime, "%Y-%m-%d")
+                  << std::setw(20) << std::put_time(localReturnTime, "%Y-%m-%d")
                   << std::endl;
     }
 }
